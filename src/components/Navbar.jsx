1cdce75e0a19670b37c66baa183cc69aed682701
AleKoles <kolesnikova.alexandra@gmail.com>
2022-08-09 11:45:07 +0300

    feat: created navbar
    @heroicons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,34 @@
-import React from "react";
-import { BeakerIcon } from "@heroicons/react/solid";
+import React, { useState } from "react";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => setNav(!nav);
   return (
-    <div>
-      <h1 className="text-2xl">
-        <strong>Dmitri Petrov</strong>
-        <span className="text-xl uppercase font-thin"> / 3D Animator</span>
-      </h1>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={2}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M4 6h16M4 12h16M4 18h16"
-        />
-      </svg>
+    <div className="w-screen h-[80px]  z-10 bg-white fixed">
+      <div className="flex justify-between px-6 items-center w-full h-full">
+        <div className="flex items-center text-xl font-bold mr-4 sm:text-2xl">
+          <h1 className="">
+            <strong>Dmitri Petrov</strong>
+            <span className="uppercase font-extralight"> / 3D Animator</span>
+          </h1>
+        </div>
+        <ul className="hidden md:flex ">
+          <li>About me</li>
+          <li>Resume</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+        <div className="md:hidden" onClick={handleNav}>
+          {!nav ? <MenuIcon className="w-6" /> : <XIcon className="w-6" />}
+        </div>
+      </div>
+      <ul className={!nav ? "hidden" : "absolute w-full px-8"}>
+        <li>About me</li>
+        <li>Resume</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
     </div>
   );
 };
