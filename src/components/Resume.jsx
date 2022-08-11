c219ade2957ee9824e05fddc6c38223c7a02db0c
AleKoles <kolesnikova.alexandra@gmail.com>
2022-08-11 23:44:17 +0300

    feat: added content to resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Resume = () => {
   return (
-    <div className="w-full h-screen bg-dark-blue absolute top-[100px]">
-      <h1 className="my-10 py-10 text-white text-4xl font-bold text-center">
+    <div className="w-full h-[2000px] bg-dark-blue absolute top-[100px]">
+      <h1 className="my-10 py-10 text-white text-5xl font-bold text-center">
         Resume
       </h1>
       <div className="flex justify-around items-center">
@@ -15,9 +15,9 @@ const Resume = () => {
         </button>
       </div>
 
-      <div className="bg-white absolute rounded shadow-lg top-[320px] w-11/12 md:w-9/12 left-[16px] sm:left-[40px] md:left-[100px] lg:left-[200px]">
-        <div className="py-2 px-4">
-          <div className="grid grid-rows-14">
+      <div className=" absolute  top-[320px] w-11/12 md:w-9/12 left-[16px] sm:left-[40px] md:left-[100px] lg:left-[200px]">
+        <div className="py-2 px-4 bg-white rounded shadow-lg">
+          <div className="grid grid-rows-6">
             <div className="row">
               <div className="col-span-8  mb-5">
                 <h2 className="text-xl md:text-2xl font-bold">Experience</h2>
@@ -34,7 +34,7 @@ const Resume = () => {
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <h3 className="text-md md:text-lg font-bold uppercase text-dark-blue">
+                    <h3 className="text-md md:text-lg font-bold uppercase text-dark-blue hover:text-light-blue">
                       Apparat Gaming
                     </h3>
                   </a>
@@ -44,10 +44,190 @@ const Resume = () => {
                     3D Animator / Modeller
                   </h2>
                   <p>
-                    Creating slot animations, visual effects, character
-                    animations; <br /> Modelling, rigging and skinning
-                    characters;
+                    Modelling, rigging and skinning characters; <br /> Creating
+                    slot animations, visual effects, and character animations.
                   </p>
+                </div>
+              </div>
+            </div>
+            <div className="row row-span-2 mt-4">
+              <div className="grid grid-cols-8">
+                <div className="col-span-3">
+                  <h2 className="text-lg md:text-xl font-bold">2017 - 2022</h2>
+                  <a
+                    href="https://rubyplay.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <h3 className="text-md md:text-lg font-bold uppercase text-dark-blue hover:text-light-blue">
+                      RubyPlay
+                    </h3>
+                  </a>
+                </div>
+                <div className="col-span-5">
+                  <h2 className="text-lg md:text-xl font-bold">
+                    Lead Animator
+                  </h2>
+                  <p>
+                    Creating slot animations, visual effects, and character
+                    animations;
+                    <br />
+                    Defining animation pipelines and workflows;
+                    <br /> Managing the animation team;
+                    <br /> Providing mentorship and feedback for junior
+                    team-members.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row mt-2">
+              <div className="grid grid-cols-8">
+                <div className="col-span-3">
+                  <h2 className="text-lg md:text-xl font-bold">2015 - 2017</h2>
+                  <a
+                    href="https://www.skywindgroup.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <h3 className="text-md md:text-lg font-bold uppercase text-dark-blue hover:text-light-blue">
+                      Skywind Group
+                    </h3>
+                  </a>
+                </div>
+                <div className="col-span-5">
+                  <h2 className="text-lg md:text-xl font-bold">
+                    2D / 3D Animator
+                  </h2>
+                  <p>
+                    Creating slot animations, visual effects, character
+                    animations
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row mt-1">
+              <div className="grid grid-cols-8">
+                <div className="col-span-3">
+                  <h2 className="text-lg md:text-xl font-bold">2014 - 2015</h2>
+                  <a
+                    href="https://solarmedia.biz/?fbclid=IwAR0bXsk68A_TZBF01JII-EhO-qRwK1AG_k3Ys3k6bMjXvrWRaUK332EVSAE"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <h3 className="text-md md:text-lg font-bold uppercase text-dark-blue hover:text-light-blue">
+                      Solar Media
+                    </h3>
+                  </a>
+                </div>
+                <div className="col-span-5">
+                  <h2 className="text-lg md:text-xl font-bold">
+                    Motion Designer
+                  </h2>
+                  <p>
+                    Creating TV idents, bumpers; <br />
+                    Program adaptation.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row md:mt-5 mb-2">
+              <div className="grid grid-cols-8">
+                <div className="col-span-3">
+                  <h2 className="text-lg md:text-xl font-bold">2011 - 2014</h2>
+                  <a
+                    href="https://www.linkedin.com/company/apparatgaming/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <h3 className="text-md md:text-lg font-bold uppercase text-dark-blue hover:text-light-blue">
+                      ERS Games
+                    </h3>
+                  </a>
+                </div>
+                <div className="col-span-5">
+                  <h2 className="text-lg md:text-xl font-bold">2D Animator</h2>
+                  <p>
+                    Creating slot animations, visual effects, and character
+                    animations
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-10 py-2 px-4 bg-white rounded shadow-lg">
+          <div>
+            <h2 className="text-xl md:text-2xl font-bold mb-10">Skills</h2>
+          </div>
+          <div>
+            <h3 className="text-lg md:text-xl font-bold mb-5">Software</h3>
+            <div className="flex justify-around items-center flex-wrap"></div>
+          </div>
+          <div>
+            <h3 className="text-lg md:text-xl font-bold mb-5">Languages</h3>
+          </div>
+        </div>
+        <div className="mt-10 py-2 px-4 bg-white rounded shadow-lg">
+          <div className="grid grid-rows-4">
+            <div className="row">
+              <div className="col-span-8  mb-5">
+                <h2 className="text-xl md:text-2xl font-bold">Education</h2>
+              </div>
+            </div>
+            <div className="row mt-2">
+              <div className="grid grid-cols-8">
+                <div className="col-span-3">
+                  <h2 className="text-lg md:text-xl font-bold">2018</h2>
+                </div>
+                <div className="col-span-5">
+                  <a
+                    href="https://ianimate.net/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <h3 className="text-md md:text-lg font-bold uppercase text-dark-blue hover:text-light-blue">
+                      iAnimate Animation School
+                    </h3>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="row mt-2">
+              <div className="grid grid-cols-8">
+                <div className="col-span-3">
+                  <h2 className="text-lg md:text-xl font-bold">2018</h2>
+                </div>
+                <div className="col-span-5">
+                  <a
+                    href="https://www.animsquad.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <h3 className="text-md md:text-lg font-bold uppercase text-dark-blue hover:text-light-blue">
+                      Animsquad Animation School
+                    </h3>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="row mt-2">
+              <div className="grid grid-cols-8">
+                <div className="col-span-3">
+                  <h2 className="text-lg md:text-xl font-bold">2005 - 2011</h2>
+                </div>
+                <div className="col-span-5">
+                  <a
+                    href="https://nau.edu.ua/en/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <h3 className="text-md md:text-lg font-bold uppercase text-dark-blue hover:text-light-blue">
+                      National Aviation University,{" "}
+                      <span className="capitalize">
+                        Network Information Security, MSc
+                      </span>
+                    </h3>
+                  </a>
                 </div>
               </div>
             </div>
