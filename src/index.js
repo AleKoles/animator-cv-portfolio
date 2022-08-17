7d9623ed230ba9b4907ca451cdaa516fc4766fcb
AleKoles <kolesnikova.alexandra@gmail.com>
2022-08-17 18:15:50 +0300

    fix: and once again

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Hero from "./components/Hero";
+import Resume from "./components/Resume";
+import Contacts from "./components/Contacts";
+import Projects from "./components/Projects";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Hero />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Route>
+    </Routes>
   </BrowserRouter>
 );
